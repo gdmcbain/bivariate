ed8859106bd1c65ccc515445b0f2ca742c7d698e
G. D. McBain <gdmcbain@protonmail.com>
2018-05-14 10:28:44 +1000

    example from Hoel

---
+++ education.marriage/education.marriage.R
@@ -0,0 +1,6 @@
+# reproducing §10.6 ‘Contingency tables’ of Hoel (1962)
+
+# cf. χ² = 12.6 (p. 255)
+tbl <- read.table("education.marriage.tab")
+tbl
+chisq.test(t(tbl), correct=FALSE) # But R gives
