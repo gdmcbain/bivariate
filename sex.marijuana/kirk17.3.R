696689e9ade9521e24c414fd1964db6a38091843
G. D. McBain <gdmcbain@protonmail.com>
2018-05-17 16:03:35 +1000

    unicode

--- sex.marijuana/kirk17.3.R
+++ sex.marijuana/kirk17.3.R
@@ -1,6 +1,6 @@
 # reproducing S. 17.3 "Testing independence" of Kirk (1978)
 
-# cf. chi^2 = 5.555 (p. 337)
+# cf. χ² = 5.555 (p. 337)
 
 tbl8.7 <- read.table("kirk17.3.tab")
 chisq.test(tbl8.7, correct=FALSE)
