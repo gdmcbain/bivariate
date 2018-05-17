76d4b34291e66a87211bc56447a9e2a889c349a6
G. D. McBain <gdmcbain@protonmail.com>
2018-05-17 14:57:04 +1000

    unicode

--- uni.income/uni.income.R
+++ uni.income/uni.income.R
@@ -1,6 +1,6 @@
 # reproducing S. 17.3 "Testing independence" of Kirk (1978)
 
-# cf. chi^2 = 17.978 (p. 339)
+# cf. χ² = 17.978 (p. 339)
 
 tbl <- read.table("uni.income.tab")
 chisq.test(tbl, correct=FALSE)	# But R gives 14.178 Hmm.
