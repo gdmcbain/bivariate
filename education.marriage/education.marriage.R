a25c9e0b733ca67beb17bb6a1e64c37ecc9bcbd6
G. D. McBain <gdmcbain@protonmail.com>
2018-05-17 14:53:39 +1000

    resolved discrepancy with Hoel #1

--- education.marriage/education.marriage.R
+++ education.marriage/education.marriage.R
@@ -1,6 +1,14 @@
 # reproducing §10.6 ‘Contingency tables’ of Hoel (1962)
 
-# cf. χ² = 12.6 (p. 255)
+# exposing a 4% error from to rounding the expected counts
+
+# cf. "It will be found that χ² = 20.7." (p. 255) where we get 19.943.
+
 tbl <- read.table("education.marriage.tab")
-tbl
-chisq.test(t(tbl), correct=FALSE) # But R gives 19.943
+expected <- rowSums(tbl) %*% t(colSums(tbl)) / sum(tbl)
+chisq0 <- sum((tbl - expected)^2 / expected)
+print(c("Raw χ² = ", chisq0))
+expected.rounded <- round(expected)
+chisq0.rounding <- sum((tbl - expected.rounded)^2 / expected.rounded)
+print(c("χ² with rounding = ", chisq0.rounding))
+chisq.test(tbl)
